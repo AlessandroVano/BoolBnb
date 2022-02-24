76c3a7ee333816d3de914d6ac0c60563d8c43e5a
AlessandroVano <alessandrovanoli.16@gmail.com>
2022-02-24 13:11:20 +0100

    initial setup

---
+++ resources/views/guests/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>BoolBnb</title>
+</head>
+<body>
+     <div>
+         <h1>Weee TRIMONEEE!!</h1>
+     </div>
+
+     <script src="{{asset('js/front-office.js')}}"></script>
+</body>
+</html>
