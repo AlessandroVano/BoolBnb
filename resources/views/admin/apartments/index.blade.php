c2efce2fea3b61488e84b7e05da147fe1822b66a
giulio30795 <giulioincarbone95@gmail.com>
2022-02-25 16:25:04 +0100

    check style

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<h1 class="text-center my-5">I tuoi Appartamenti</h1>
+<h1 class="text-center my-5">Your Apartments</h1>
 
 <div class="container">
     @if (session('deleted'))
@@ -10,44 +10,43 @@
                 <strong>
                     {{ session('deleted') }}
                 </strong>
-                    Cancellato Con Successo
+                    Deleted Successfully!
             </div>
         @endif
     <div class="row">
         @foreach ($apartments as $apartment )
 
-        <div class="col border p-1 mx-2">
-            <h3 class="text-center">{{$apartment->name}}</h3>
+        <div class=" col-12 col-md-6 col-lg-4 my-3">
+            <div class="card-container border p-2 mx-2">
+                <h3 class="text-center">{{$apartment->name}}</h3>
 
-            <div class=" text-center img-container">
-                <img src="{{$apartment->image}}" alt="{{$apartment->name}}">
-            </div>
-            <div class=" p-1 text-center detail-container my-3">
-                <h5>
-                    @if($apartment->visibility)
-                        Apartment Available
-                    @else Apartment NOT Available
-                    @endif
-                </h5>
-                <div class="actions d-flex justify-content-around ">
+                <div class="text-center img-container">
+                    <img class="w-100"src="{{$apartment->image}}" alt="{{$apartment->name}}">
+                </div>
+                <div class="p-1 text-center detail-container my-3">
+                    <h5>
+                        @if($apartment->visibility)
+                            Apartment Available
+                        @else Apartment NOT Available
+                        @endif
+                    </h5>
+                    <div class="actions d-flex justify-content-around ">
 
-                    <button class="btn btn-success">
-                        <a class="text-decoration-none text-white"
-                        href="{{route('admin.apartments.show', $apartment->id)}}">View Details</a>
-                    </button>
-                    
-                    <form action="{{route('admin.apartments.destroy', $apartment->id)}}" method="POST">
+                        <button class="btn btn-success">
+                            <a class="text-decoration-none text-white"
+                            href="{{route('admin.apartments.show', $apartment->id)}}">View Details</a>
+                        </button>
                         
-                        @csrf
-                        @method('DELETE')
-                        
-                        <button type="submit" class="btn btn-danger">Delete Apartment</button>
-                        
-                    </form>
+                        <form action="{{route('admin.apartments.destroy', $apartment->id)}}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            
+                            <button type="submit" class="btn btn-danger">Delete Apartment</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-
         @endforeach
     </div>
 </div>
