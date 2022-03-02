fc6afd95d69cc3fdc3a072fb14fa6306846c5998
DanyMencu <dany69men@gmail.com>
2022-03-02 12:07:58 +0100

    Add header logo

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,7 +1,8 @@
 <template>
     <header class="mb-5">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <img class="icon" src="../../../public/storage/icon/Logo-icon-color.png" alt="Logo Icon">
+            <a class="navbar-brand" href="#">BoolBnB</a>
             <button
                 class="navbar-toggler"
                 type="button"
