e5eb7d71ca219b728b9e55a2b799abdd60c2e37d
giulio30795 <giulioincarbone95@gmail.com>
2022-03-04 10:40:24 +0100

    fix tomtom

--- public/js/tomtom.js
+++ public/js/tomtom.js
@@ -8,21 +8,6 @@ let array = [];
 
 inputAddress.addEventListener('keyup', getGps);
 
-let mappa = document.getElementById('mappa');
-var options = {
-    searchOptions: {
-        key: "ue74ZxVT9w3YLf0sEeYAz5GOv1v6G1md",
-        language: "en-GB",
-        limit: 5,
-    },
-    autocompleteOptions: {
-        key: "ue74ZxVT9w3YLf0sEeYAz5GOv1v6G1md",
-        language: "en-GB",
-    },
-};
-var ttSearchBox = new tt.plugins.SearchBox(tt.services, options);
-var searchBoxHTML = ttSearchBox.getSearchBoxHTML();
-mappa.append(searchBoxHTML);
 function getGps(){
     let li = document.getElementsByTagName('li');
 
@@ -67,5 +52,4 @@ function getGps(){
             contenitore.addEventListener('click', setValue)
         })
     });
-
 }
