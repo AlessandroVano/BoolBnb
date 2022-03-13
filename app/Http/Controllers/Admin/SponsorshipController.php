eb56ff74170c550f530a2e921c705bbf35d57127
DanyMencu <dany69men@gmail.com>
2022-03-13 12:17:04 +0100

    Fix

--- app/Http/Controllers/Admin/SponsorshipController.php
+++ app/Http/Controllers/Admin/SponsorshipController.php
@@ -19,7 +19,7 @@ class SponsorshipController extends Controller
         return view('admin.sponsorships.index', compact('sponsorships', 'apartment'));
     }
 
-    public function sponsorizza($sponsorship) {
+    //public function sponsorizza($sponsorship) {
 
         //Salvo in una variabile tutti i dati ritornati
         //$data = $request->all();
@@ -27,40 +27,37 @@ class SponsorshipController extends Controller
         // dd($sponsorship);
 
         //Salvo la data odierna
-        $now = Carbon::now();
+        //$now = Carbon::now();
 
-        $checkApartment = ApartmentSponsorship::where('apartment_id', $request->apartment_id);
+        //$checkApartment = ApartmentSponsorship::where('apartment_id', $request->apartment_id);
 
-        if ( !$checkApartment ) {
+        //if ( !$checkApartment ) {
             //Controllo sul DB se esiste un record già esistente e mi salvo la data di fine più "lontana"
-            $lastEndDate = Carbon::parse(ApartmentSponsorship::where('apartment_id', $request->apartment_id)->pluck('end_date')->sortDesc()->first());
+            //$lastEndDate = Carbon::parse(ApartmentSponsorship::where('apartment_id', $request->apartment_id)->pluck('end_date')->sortDesc()->first());
     
             //Controllo se la data di fine più "lontana" è già passata o non
-            if ($lastEndDate->greaterThan($now)) {
-                $request['start_date'] = $lastEndDate;
-            } else {
-                $request['start_date'] = $now;
-            };
-        } else {
-            $request['start_date'] = $now;
-        }
+            //if ($lastEndDate->greaterThan($now)) {
+                //$request['start_date'] = $lastEndDate;
+            //} else {
+                //$request['start_date'] = $now;};
+        //} else {
+            //$request['start_date'] = $now;}
 
         //Aggiungo durata alla data inizio e calcolo data fine
-        $duration = Sponsorship::where('id', $request->sponsorship_id)->pluck('duration')->first();
+        //$duration = Sponsorship::where('id', $request->sponsorship_id)->pluck('duration')->first();
 
-        $request['end_date'] = Carbon::parse($request['start_date'])->addHours($duration);
+        //$request['end_date'] = Carbon::parse($request['start_date'])->addHours($duration);
 
         //Salvo i dati in DB
-        $ApartmentSponsorship = ApartmentSponsorship::create($data);
+        //$ApartmentSponsorship = ApartmentSponsorship::create($data);
         /* $ApartmentSponsorship = new ApartmentSponsorship;
         $ApartmentSponsorship->fill($data);
         $ApartmentSponsorship->save(); */
 
         //Trovo l'appartamento per prendere lo slug
-        $apartment = Apartment::where('id', $request->apartment_id)->first();
+        //$apartment = Apartment::where('id', $request->apartment_id)->first();
 
-        return redirect()->route('admin.apartments.show', $apartment['apartment_id']);
-    }
+        //return redirect()->route('admin.apartments.show', $apartment['apartment_id']);}
 
 
     public function checkout($id) {
